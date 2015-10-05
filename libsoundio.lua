82b92e0cf4ea8051afbdcd0e9d61fa1fd38fe5bf
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-10-05 06:30:02 +0000

    unimportant

--- libsoundio.lua
+++ libsoundio.lua
@@ -113,14 +113,17 @@ sio.wakeup = C.soundio_wakeup
 
 sio.force_device_scan = C.soundio_force_device_scan
 
-local function devices_api(get, count, getdefault)
+local function devices_api(getbyindex, count, getdefaultindex)
+	local function get(self, i)
+		return ffi.gc(checkptr(getbyindex(self, i)), C.soundio_device_unref)
+	end
 	local function api(self, which, filter)
 		if not which then --iterate filtered
 			local i = 0
 			local n = count(self)
 			return function()
 				while i < n do
-					local dev = checkptr(get(self, i))
+					local dev = get(self, i)
 					i = i + 1
 					if filter(dev) then
 						return dev
@@ -139,8 +142,8 @@ local function devices_api(get, count, getdefault)
 			return n
 		elseif which == '*' then --get default
 			if filter == nil then
-				local i = getdefault(self)
-				return i >= 0 and checkptr(get(self, i)) or nil
+				local i = getdefaultindex(self)
+				return i >= 0 and get(self, i) or nil
 			end
 			--get default filtered
 			local dev = api(self, '*')
@@ -267,7 +270,7 @@ local layoutprop = {}
 layout.__eq = C.soundio_channel_layout_equal
 layout.__index = vprops(layout, layoutprop)
 
-function M.layouts(which, channel_count)
+function M.builtin_layouts(which, channel_count)
 	if not which then --iterate
 		local i = -1
 		local n = C.soundio_channel_layout_builtin_count()
@@ -283,8 +286,11 @@ function M.layouts(which, channel_count)
 	end
 end
 
-function layout:find_channel(channel_id)
-	local i = C.soundio_channel_layout_find_channel(self, channel_id)
+function layout:find_channel(channel)
+	if type(channel) == 'string' then
+		channel = assert(M.channel_id(channel), 'invalid channel name')
+	end
+	local i = C.soundio_channel_layout_find_channel(self, channel)
 	return i ~= -1 and i or nil
 end
 
@@ -298,10 +304,6 @@ end
 
 function dev:sort_layouts()
 	C.soundio_sort_channel_layouts(self.layouts, self.layout_count)
-end
-
-function M.best_matching_channel_layout(preferred, available)
-	return C.soundio_best_matching_channel_layout(preferred, #preferred, available, #available)
 end
 
 --streams --------------------------------------------------------------------
