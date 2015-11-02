00009b9be0a5d5cd7af77cfddbd5e842da5c2a2e
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-11-02 20:30:02 +0000

    unimportant

--- csrc/minizip/build.sh
+++ csrc/minizip/build.sh
@@ -1,4 +1,5 @@
+export PATH="/home/cosmin/osxcross/target/bin:$PATH"
 gcc -c -O2 $C zip.c unzip.c ioapi.c -I. -I../zlib
 gcc *.o -shared -o ../../bin/$P/$D -L../../bin/$P -lz $L
-ar rcs ../../bin/$P/$A *.o
+#o64-ar rcs ../../bin/$P/$A *.o
 rm *.o
