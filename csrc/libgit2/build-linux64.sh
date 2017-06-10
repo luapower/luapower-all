e5d1be8fb3603ff10557c6f1c73649d35c0a8579
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2017-06-10 15:30:02 +0100

    unimportant

--- csrc/libgit2/build-linux64.sh
+++
@@ -1,1 +0,0 @@
-C="src/unix/*.c -fPIC -include _memcpy.h" P=linux64 L="-s -static-libgcc -lrt" D=libgit2.so A=libgit2.a ./build.sh
--- csrc/wluajit/build-mingw64.sh
+++
@@ -1,5 +0,0 @@
-LUAJIT_SRC=../luajit/src/src
-BIN_DIR=../../bin/mingw64
-windres luajit.rc luajit.o
-gcc -O2 -s -static-libgcc wmain.c luajit.o $LUAJIT_SRC/luajit.c -o $BIN_DIR/wluajit.exe -mwindows -llua51 -I$LUAJIT_SRC -L$BIN_DIR
-rm luajit.o
