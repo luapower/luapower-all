f3c5cb8d5123c2eab4889d41745fab5a7c1ef3c5
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2018-09-24 15:30:03 +0100

    unimportant

--- .mgit/bundle.sh
+++ .mgit/bundle.sh
@@ -127,6 +127,7 @@ compile_dasl_module() {
 
 # usage: f=file.* [name=file.*] o=file.o $0 CFLAGS... -> file.o
 compile_bin_module() {
+	local name=$name
 	[ "$name" ] || name=$f
 	local sym=${name//[\-\.\/\\]/_}  # foo/bar-baz.ext -> foo_bar_baz_ext
 	sym=$BBIN_PREFIX$sym compile_bin_file "$@"
