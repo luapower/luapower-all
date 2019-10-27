e6336b888f4c50dcd5c8ad8ba6e38463b77d1638
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2019-10-27 21:30:01 +0000

    unimportant

--- csrc/luajit/build-linux64.sh
+++ csrc/luajit/build-linux64.sh
@@ -1,4 +1,4 @@
-export LUA_PATH_DEFAULT="./?.lua;!/?.lua;!/../../?.lua;!/../../?/init.lua"
+export LUA_PATH_DEFAULT="./?.lua;!/lua/?.lua;!/lua/?/init.lua;!/../../?.lua;!/../../?/init.lua"
 export LUA_CPATH_DEFAULT="./?.so;!/clib/?.so;!/loadall.so"
 MAKE=make P=linux64 CFLAGS="-pthread" \
 	LDFLAGS="-pthread -s -static-libgcc -Wl,-rpath,'\$\$ORIGIN'" \
