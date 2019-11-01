3eeea8beca6036ddd0c4f0c07c57195ebff9104f
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2019-11-01 16:30:02 +0000

    unimportant

--- csrc/socket/build.sh
+++ csrc/socket/build.sh
@@ -1,5 +1,3 @@
-(cd src && echo ../*.patch | patch -q -p1)
-
 mkdir -p "$(dirname ../../bin/$P/clib/socket/$SD)"
 ${X}gcc -c -O2 $C $files -I. -I../lua-headers
 ${X}gcc *.o -shared -o ../../bin/$P/clib/socket/$SD $L
