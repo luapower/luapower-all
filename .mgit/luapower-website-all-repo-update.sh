aca1e4d4b0b663369cae3f8b2bd646c246ddf71e
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2019-12-26 23:04:30 +0000

    unimportant

--- .mgit/luapower-website-all-repo-update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# push the all-in-one luapower repo at github.com/luapower/luapower-all
-cd ~/luapower
-git up
--- .mgit/luapower-website-clear-cache.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ "$1" ]; then
-    wget -qO- https://luapower.com/$1/cc
-else
-    wget -qO- https://luapower.com/clear_cache
-fi
