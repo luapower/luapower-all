3bb582253b1cfe61f5c14be144cb24f7a523aac1
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2020-07-17 00:30:03 +0100

    unimportant

--- donut_studio.lua
+++ donut_studio.lua
@@ -5,5 +5,8 @@ require'webb_action'
 config('root_action', 'home')
 
 return function()
+	if args()[1] == 'comanda' or args()[1] == 'multumim' then
+		args()[1] = ''
+	end
 	check(action(find_action(unpack(args()))))
 end
