9288b2e60ac4a492b88d1546f7ed88838be0c016
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2020-07-17 16:30:03 +0100

    unimportant

--- donut_studio.lua
+++ donut_studio.lua
@@ -2,11 +2,16 @@ require'$'
 require'webb'
 require'webb_action'
 
-config('root_action', 'home')
+config('root_action', 'shop')
+
+function action.plaseaza_comanda()
+	local order = post()
+	--writefile('donut-studio/'+
+end
 
 return function()
 	if args()[1] == 'comanda' or args()[1] == 'multumim' then
-		args()[1] = ''
+		args()[1] = 'shop'
 	end
 	check(action(find_action(unpack(args()))))
 end
